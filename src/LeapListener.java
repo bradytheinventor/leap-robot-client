57411759ec3b7156f6ad5147ccee7aad34b76cf0
bradytheinventor <bcra1234@gmail.com>
2017-06-24 17:43:00 -0500

    Cleared up deadband calculation logic
    Cleared up logic to apply deadbands and output cap.

--- src/LeapListener.java
+++ src/LeapListener.java
@@ -23,24 +23,22 @@ public class LeapListener extends Listener {
 		}
 		
 		for(Hand h : frame.hands()) {
-			String s = h.isLeft() ? "left" : "right";
 			Vector d = h.direction();
 			Vector p = h.palmPosition();
 			
 			double pitch = d.pitch() * RAD_TO_DEG;
-			//double roll = d.roll() * RAD_TO_DEG;
-			//double yaw = d.yaw() * RAD_TO_DEG;
-			
-			double x = p.getX();
-			double y = p.getY();
 			double z = p.getZ();
 			
-			double sign = Math.signum(z);
-			double power = (z - (sign * MIN_Z)) / (MAX_Z - MIN_Z);
-			power *= -1.0;
+			if(Math.abs(pitch) < MIN_PITCH) {
+				continue;
+			}
 			
-			power = Math.abs(pitch) > MIN_PITCH ? power : 0.0;
-			power = Math.abs(z) > MIN_Z ? power : 0.0;
+			if(Math.abs(z) < MIN_Z) {
+				continue;
+			}
+			
+			double sign = Math.signum(z);
+			double power = -((z - (sign * MIN_Z)) / (MAX_Z - MIN_Z));
 			power = Math.abs(z) > MAX_Z ? 1.0 : power;
 			
 			if(h.isLeft()) {
@@ -48,9 +46,6 @@ public class LeapListener extends Listener {
 			} else {
 				output[1] = power;
 			}
-			
-			//System.out.println("hand " + s + ", p: " + pitch);
-			System.out.println(" z: " + p.getZ() + " out: " + power);
 		}
 		
 		System.out.println(output[0] + " " + output[1]);
