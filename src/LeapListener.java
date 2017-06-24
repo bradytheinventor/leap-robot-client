8048c2e353561b0f2cd0f896b58f355da1bc158c
bradytheinventor <bcra1234@gmail.com>
2017-06-24 16:17:26 -0500

    Added deadband and pitch-to-output power conversion
    Added deadband zone, when pitch is within MIN_PITCH of 0, output power
    is 0. When pitch is between MIN_PITCH and MAX_PITCH, output power scales
    from 0 to 1.

--- src/LeapListener.java
+++ src/LeapListener.java
@@ -16,15 +16,30 @@ public class LeapListener extends Listener {
 	public void onFrame(Controller controller) {
 		System.out.println("frame");
 		Frame frame = controller.frame();
+		double[] output = {0.0, 0.0};
+		
 		if(frame.hands().count() < 1) {
 			return;
 		}
+		
 		for(int h = 0; h < frame.hands().count(); h++) {
 			String s = frame.hands().get(h).isLeft() ? "left" : "right";
+			Vector d = frame.hands().get(h).direction();
+			
 			double pitch = d.pitch() * RAD_TO_DEG;
 			//double roll = d.roll() * RAD_TO_DEG;
 			//double yaw = d.yaw() * RAD_TO_DEG;
+			
+			double sign = Math.signum(pitch);
+			output[h] = (pitch - (sign * MIN_PITCH)) / (MAX_PITCH - MIN_PITCH);
+			output[h] *= -1.0;
+			
+			output[h] = Math.abs(pitch) > MIN_PITCH ? output[h] : 0.0;
+			output[h] = Math.abs(pitch) > MAX_PITCH ? 1.0 : output[h];
+			
 			System.out.println("hand " + s + ", p: " + pitch);
 		}
+		
+		System.out.println(output[0] + " " + output[1]);
 	}
 }
