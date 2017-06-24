fe4659c505a1c706dcfd871b8190510addfad72e
bradytheinventor <bcra1234@gmail.com>
2017-06-24 16:09:22 -0500

    Added constants and pitch testing code
    Added pitch conversion constants and debug printing.

--- src/LeapListener.java
+++ src/LeapListener.java
@@ -1,6 +1,11 @@
 import com.leapmotion.leap.*;
 
 public class LeapListener extends Listener {
+	//declare constants
+	private final double RAD_TO_DEG = 180 / Math.PI;
+	
+	private final double MIN_PITCH = 10.0;
+	private final double MAX_PITCH = 30.0;
 	
 	@Override
 	public void onConnect(Controller controller) {
@@ -10,5 +15,16 @@ public class LeapListener extends Listener {
 	@Override
 	public void onFrame(Controller controller) {
 		System.out.println("frame");
+		Frame frame = controller.frame();
+		if(frame.hands().count() < 1) {
+			return;
+		}
+		for(int h = 0; h < frame.hands().count(); h++) {
+			String s = frame.hands().get(h).isLeft() ? "left" : "right";
+			double pitch = d.pitch() * RAD_TO_DEG;
+			//double roll = d.roll() * RAD_TO_DEG;
+			//double yaw = d.yaw() * RAD_TO_DEG;
+			System.out.println("hand " + s + ", p: " + pitch);
+		}
 	}
 }
