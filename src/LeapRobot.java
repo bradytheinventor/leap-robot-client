0811a2238d7be7967281751db81d217e89499dce
bradytheinventor <bcra1234@gmail.com>
2017-06-24 17:15:17 -0500

    Added SmartDashboard connection code
    Added code to connect to a FIRST robot with team number 2823.

--- src/LeapRobot.java
+++ src/LeapRobot.java
@@ -10,6 +10,35 @@ public class LeapRobot {
 		
 		controller.addListener(listener);
 		
+		System.out.print("Press ENTER to connect to robot");
+		
+		try {
+			System.in.read();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		//set computer as NetworkTables client
+		System.out.println("[NetworkTables] Setting Client mode");
+		NetworkTable.setClientMode();
+		
+		//set IP address to mDNS address
+		System.out.println("[NetworkTables] Setting IP address");
+		NetworkTable.setIPAddress("roborio-2823-frc.local");
+		
+		//try to initialize NetworkTable while ignoring any errors
+		System.out.println("[NetworkTables] Initializing...");
+		NetworkTable.initialize();
+		
+		//attempt to connect to SmartDashboard NetworkTable
+		System.out.println("[NetworkTables] Getting SmartDashboard table...");
+		NetworkTable SmartDashboard = NetworkTable.getTable("SmartDashboard");
+		
+		//print out status of SmartDashboard connection
+		System.out.println("[SmartDashboard] Connected: " + SmartDashboard.isConnected());
+		
+		System.out.println("[NetworkTables] Setup complete (but SmartDashboard may be unavailable)");
+		
 		System.out.println("Press ENTER to quit");
 		
 		try {
